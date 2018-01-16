8c11d48990b9bc1e727695058765bd54e5a23050
su <su@revoke.cc>
2018-01-16 20:00:30 +0800

    dev

--- static/js/layout.js
+++ static/js/layout.js
@@ -3,8 +3,19 @@ layui.define(['layer', 'element'], function(exports) {
         layer = layui.layer,
         $ = layui.$;
 
+    //监听侧栏缩进
     $('.lying-nav-fold').click(function () {
         $('.layui-side').toggleClass('lying-side-mini');
+    });
+
+    //监听菜单展开
+    $('.lying-nav-header').click(function () {
+        $(this).parent().toggleClass('lying-nav-open').siblings('.lying-nav-item').removeClass('lying-nav-open');
+    });
+
+    //MINI菜单下显示tips
+    $(document).on('mouseenter', '.layui-side.lying-side-mini .lying-nav-item a', function () {
+        layer.tips($(this).find('.lying-nav-title').text(), this, {time: 1000, tips: [2, '#53616F']});
     });
 
     exports('layout', {});
