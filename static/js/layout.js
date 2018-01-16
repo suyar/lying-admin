db6e5c88c8a9aba8c7c926fb475f56f0d9eb7f32
su <su@revoke.cc>
2018-01-17 00:06:58 +0800

    dev

--- static/js/layout.js
+++ static/js/layout.js
@@ -3,13 +3,256 @@ layui.define(['layer', 'element'], function(exports) {
         layer = layui.layer,
         $ = layui.$;
 
+    var layout = function () {
+        this.offset = 0;
+        this.titleLen = 0;
+        this.tabLen = 0;
+        this.diff = 0;
+        this.title = $('.layui-tab-title');
+        this.filter = $('.layui-body .layui-tab').attr('lay-filter');
+    };
+
+    /**
+     * 获取标题栏宽度
+     * @returns {layout}
+     */
+    layout.prototype.getTitleLen = function () {
+        this.titleLen = this.title.width();
+        return this;
+    };
+
+    /**
+     * 计算所有tab宽度
+     * @returns {layout}
+     */
+    layout.prototype.getTabLen = function () {
+        var _this = this;
+        _this.tabLen = 0;
+        $('.layui-tab-title li').each(function () {
+            _this.tabLen += $(this).outerWidth();
+        });
+        return _this;
+    };
+
+    /**
+     * 计算标题栏宽度和所有tab的宽度差
+     * @returns {layout}
+     */
+    layout.prototype.getDiff = function () {
+        this.diff = this.titleLen - this.tabLen;
+        return this;
+    };
+
+    /**
+     * 设置tab偏移
+     * @param offset 偏移量
+     * @returns {layout}
+     */
+    layout.prototype.setOffset = function (offset) {
+        offset = offset || this.offset;
+        if (offset > 0 || this.diff > 0) {
+            this.offset = offset = 0;
+        } else if (offset < 0 && offset < this.diff) {
+            this.offset = offset = this.diff;
+        }
+        this.title.css('left', offset + 'px');
+        return this;
+    };
+
+    /**
+     * 大小改变重新计算参数
+     * @returns {layout}
+     */
+    layout.prototype.resize = function () {
+        this.getTitleLen().getDiff();
+        if (this.diff > 0) {
+            this.setOffset(this.offset = 0);
+        } else {
+            var _tab = $('.layui-tab-brief>.layui-tab-title .layui-this'),
+                _tabWidth = _tab.outerWidth(),
+                _tabPos = _tab.position().left,
+                d;
+
+            if (this.offset < -_tabPos) {
+                this.setOffset(this.offset = -_tabPos);
+            } else if ((d = this.titleLen - _tabPos - _tabWidth) < 0) {
+                this.setOffset(this.offset = d);
+            } else if (this.offset < this.diff) {
+                this.setOffset(this.offset = this.diff);
+            }
+        }
+        return this;
+    };
+
+    /**
+     * 延迟执行resize
+     * @param time 延迟的时间(ms),默认200ms
+     * @returns {layout}
+     */
+    layout.prototype.delayResize = function (time) {
+        var _this = this;
+        setTimeout(function () {
+            _this.resize();
+        }, time || 200);
+        return _this;
+    };
+
+    /**
+     * 左移
+     * @returns {layout}
+     */
+    layout.prototype.goleft = function () {
+        this.setOffset(this.offset += this.titleLen);
+        return this;
+    };
+
+    /**
+     * 右移
+     * @returns {layout}
+     */
+    layout.prototype.goright = function () {
+        this.setOffset(this.offset -= this.titleLen);
+        return this;
+    };
+
+    /**
+     * 新建tab标签
+     * @param src 链接
+     * @param title 标题
+     * @param icon 字体图标
+     * @returns {layout}
+     */
+    layout.prototype.tabAdd = function (src, title, icon) {
+        if ($('li[lay-id="' + src + '"]').length === 0) {
+            if (icon) {
+                if (/^&\S+;$/.test(icon)) {
+                    title += ' <i class="layui-icon">' + icon + '</i>';
+                } else {
+                    title += ' <i class="layui-icon ' + icon + '"></i>';
+                }
+            }
+            element.tabAdd(this.filter, {
+                title: title,
+                content: '<iframe src="' + src + '"></iframe>',
+                id: src
+            });
+        }
+        element.tabChange(this.filter, src);
+        this.getTabLen().resize();
+        return this;
+    };
+
+    /**
+     * 删除非选中的选项卡
+     * @returns {layout}
+     */
+    layout.prototype.tabCloseSiblings = function () {
+        var _this = this;
+        $('.layui-tab-title li:not(.layui-this)').each(function () {
+            var id = $(this).attr('lay-id');
+            id && element.tabDelete(_this.filter, id);
+        });
+        return _this;
+    };
+
+    /**
+     * 删除第一个tab外的所有tab
+     * @returns {layout}
+     */
+    layout.prototype.tabCloseAll = function () {
+        var _this = this;
+        $('.layui-tab-title li:not(:first)').each(function () {
+            var id = $(this).attr('lay-id');
+            id && element.tabDelete(_this.filter, id);
+        });
+        return _this;
+    };
+
+    /**
+     * 删除第N个tab
+     * @param index tab的索引,从0开始,但是0不可删除
+     * @returns {layout}
+     */
+    layout.prototype.tabCloseEq = function (index) {
+        var el = $('.layui-tab-title li:eq(' + index + ')');
+        if (index && el.length) {
+            var id = el.attr('lay-id');
+            id && element.tabDelete(this.filter, id);
+        }
+        return this;
+    };
+
+    /**
+     * 根据SRC删除选项卡
+     * @param src 选项卡的SRC
+     * @returns {layout}
+     */
+    layout.prototype.tabCloseSrc = function (src) {
+        element.tabDelete(this.filter, src);
+        return this;
+    };
+
+    /**
+     * 根据SRC切换tab
+     * @param src 选项卡的SRC
+     * @returns {layout}
+     */
+    layout.prototype.tabChange = function (src) {
+        element.tabChange(this.filter, src);
+        return this;
+    };
+
+    /**
+     * 刷新当前选项卡
+     * @returns {layout}
+     */
+    layout.prototype.reload = function () {
+        var iframe = $('.layui-tab-item.layui-show').find('iframe');
+        iframe.length && iframe.attr('src', iframe.attr('src'));
+        return this;
+    };
+
+    //实例化对象
+    var obj = new layout();
+    obj.getTitleLen().getTabLen().getDiff();
+
+    //判断左侧菜单是否要展开
+    var side = layui.data('nav-side'), sideObj = $('.layui-layout-admin .layui-side');
+    if (side) {
+        var mini = sideObj.hasClass('lying-side-mini');
+        if (side.open && mini) {
+            sideObj.removeClass('lying-side-mini') && obj.delayResize();
+        } else if (!side.open && !mini) {
+            sideObj.addClass('lying-side-mini') && obj.delayResize();
+        }
+    }
+
+    //监听窗口大小改变
+    $(window).resize(function () {
+        obj.resize();
+    });
+
+    //监听关闭选项卡
+    element.on('tabDelete(' + obj.filter + ')', function(data) {
+        obj.getTabLen().resize();
+    });
+
+    //监听选项卡切换
+    element.on('tab(' + obj.filter + ')', function(data) {
+        var src = $(this).attr('lay-id'), pmenu = $('.lying-nav-child [lying-src="' + src + '"]').parents('.lying-nav-item');
+        console.log(pmenu.hasClass('lying-nav-open'));
+        pmenu.length && (pmenu.hasClass('lying-nav-open') || pmenu.trigger('click'));
+    });
+
     //监听侧栏缩进
-    $('.lying-nav-fold').click(function () {
-        $('.layui-side').toggleClass('lying-side-mini');
+    $(document).on('click', '.lying-nav-fold', function () {
+        sideObj.toggleClass('lying-side-mini');
+        layui.data('nav-side', {key: 'open', value: !sideObj.hasClass('lying-side-mini')});
+        obj.delayResize();
     });
 
     //监听菜单展开
-    $('.lying-nav-header').click(function () {
+    $(document).on('click', '.lying-nav-header', function () {
         $(this).parent().toggleClass('lying-nav-open').siblings('.lying-nav-item').removeClass('lying-nav-open');
     });
 
@@ -18,5 +261,26 @@ layui.define(['layer', 'element'], function(exports) {
         layer.tips($(this).find('.lying-nav-title').text(), this, {time: 1000, tips: [2, '#53616F']});
     });
 
-    exports('layout', {});
+    //刷新iframe
+    $(document).on('click', '.lying-tab-goleft', function () {
+        obj.goleft();
+    });
+
+    //刷新iframe
+    $(document).on('click', '.lying-tab-goright', function () {
+        obj.goright();
+    });
+
+    //刷新iframe
+    $(document).on('click', '.lying-tab-refresh', function () {
+        obj.reload();
+    });
+
+    //锚点打开选项卡
+    $(document).on('click', '[lying-src]', function () {
+        var src = $(this).attr('lying-src');
+        obj.tabAdd(src, 'nihao');
+    });
+
+    exports('layout', obj);
 });
