0f7706f1657b843e22cb7220ef6edc3f0aaaef21
su <su@revoke.cc>
2018-03-28 13:37:05 +0800

    优化目录结构,优化a标签的鼠标样式

---
+++ static/lau/lau.js
@@ -0,0 +1,10 @@
+layui.define(['layer', 'element', 'carousel'], function(exports) {
+    var element = layui.element,
+        carousel = layui.carousel,
+        layer = layui.layer,
+        $ = layui.$;
+
+
+
+    exports('lau', {});
+});
