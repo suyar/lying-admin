bb39d0804e5a9e41e03806583ab24d6b89feadfc
su <su@revoke.cc>
2018-03-29 01:19:22 +0800

    重写控制台

--- static/lau/lau.js
+++ static/lau/lau.js
@@ -4,6 +4,19 @@ layui.define(['layer', 'element', 'carousel'], function(exports) {
         layer = layui.layer,
         $ = layui.$;
 
+    //激活轮播
+    $('.lau-carousel').each(function () {
+        var that = $(this);
+        carousel.render({
+            elem: this,
+            width: '100%',
+            arrow: 'none',
+            interval: that.data('interval'),
+            autoplay: that.data('autoplay') === true,
+            trigger: 'hover',
+            anim: that.data('anim')
+        });
+    });
 
 
     exports('lau', {});
