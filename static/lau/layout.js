24cce1dcdc099c517e5add102625354221a440dc
su <su@revoke.cc>
2018-04-04 18:00:37 +0800

    重构layout

--- static/lau/layout.js
+++ static/lau/layout.js
@@ -1,32 +1,356 @@
-layui.define(['layer', 'element', 'laytpl'], function(exports) {
-    var element = layui.element,
-        layer = layui.layer,
+layui.define(['layer', 'laytpl'], function(exports) {
+    var layer = layui.layer,
         laytpl = layui.laytpl,
         $ = layui.$;
 
-    //侧栏菜单的模板,用于重新渲染
-    var sideMenuTpl = [
-        '{{# layui.each(d, function(index, item) { }}',
-        '<li class="lau-nav-item">',
-            '<a class="lau-nav-header" href="javascript:;" {{ item.href ? \'lau-href="\' + item.href + \'"\' : \'\' }} {{ item.id ? \'lau-id="\' + item.id + \'"\' : \'\' }}>',
-                '<i class="{{ item.icon || \'layui-icon layui-icon-right\' }}"></i>',
-                '<cite>{{ item.title }}</cite>',
-            '</a>',
-            '{{# if (item.list) { }}',
-            '<dl class="lau-nav-child">',
-                '{{# layui.each(item.list, function(index2, item2) { }}',
-                '<dd>',
-                    '<a href="javascript:;" {{ item2.href ? \'lau-href="\' + item2.href + \'"\' : \'\' }} {{ item2.id ? \'lau-id="\' + item2.id + \'"\' : \'\' }}>',
-                        '<i class="{{ item2.icon || \'layui-icon layui-icon-danxuankuanghouxuan\' }}"></i>',
-                        '<cite>{{ item2.title }}</cite>',
-                    '</a>',
-                '</dd>',
-                '{{# }); }}',
-            '</dl>',
-            '{{# } }}',
-        '</li>',
-        '{{# }); }}'
-    ].join('');
+    /**
+     * 模板
+     * @type {{sideMenu: string, bodyTab: string, bodySingle: string}}
+     */
+    var Tpl = {
+        sideMenu: [
+            '{{# layui.each(d, function(index, item) { }}',
+            '<li class="lau-nav-item">',
+                '<a class="lau-nav-header" href="javascript:;" {{ item.href ? \'lau-href="\' + item.href + \'"\' : \'\' }} {{ item.id ? \'lau-id="\' + item.id + \'"\' : \'\' }}>',
+                    '<i class="{{ item.icon || \'layui-icon layui-icon-right\' }}"></i>',
+                    '<cite>{{ item.title }}</cite>',
+                '</a>',
+                '{{# if (item.list) { }}',
+                '<dl class="lau-nav-child">',
+                    '{{# layui.each(item.list, function(index2, item2) { }}',
+                    '<dd>',
+                        '<a href="javascript:;" {{ item2.href ? \'lau-href="\' + item2.href + \'"\' : \'\' }} {{ item2.id ? \'lau-id="\' + item2.id + \'"\' : \'\' }}>',
+                            '<i class="{{ item2.icon || \'layui-icon layui-icon-danxuankuanghouxuan\' }}"></i>',
+                            '<cite>{{ item2.title }}</cite>',
+                        '</a>',
+                    '</dd>',
+                    '{{# }); }}',
+                '</dl>',
+                '{{# } }}',
+            '</li>',
+            '{{# }); }}'
+        ].join(''),
+        bodyTab: [
+            '<div class="layui-icon lau-tabs-ctrl layui-icon-prev"></div>',
+            '<div class="layui-icon lau-tabs-ctrl layui-icon-next"></div>',
+            '<div class="layui-icon lau-tabs-ctrl layui-icon-refresh-3"></div>',
+            '<div class="layui-icon lau-tabs-ctrl layui-icon-down">',
+                '<ul class="layui-nav lau-tabs-more" lay-filter="lau-tabs-more">',
+                    '<li class="layui-nav-item" lay-unselect>',
+                        '<a></a>',
+                        '<dl class="layui-nav-child layui-anim-fadein">',
+                            '<dd><a class="lau-tabs-close-this">关闭当前标签页</a></dd>',
+                            '<dd><a class="lau-tabs-close-siblings">关闭其它标签页</a></dd>',
+                            '<dd><a class="lau-tabs-close-all">关闭全部标签页</a></dd>',
+                        '</dl>',
+                    '</li>',
+                '</ul>',
+            '</div>',
+            '<div class="layui-tab layui-tab-brief" lay-allowClose="true" lay-filter="lau-tabs">',
+                '<ul class="layui-tab-title">',
+                    '<li class="layui-this"><i class="{{ d.icon ? (d.icon.split(\'/\s+/\').length > 1 ? d.icon : \'layui-icon \' + d.icon) : \'layui-icon layui-icon-home\' }}"></i> {{ d.title || \'控制台\' }}</li>',
+                    '<li> 关闭全部标签页关闭全部标签页关闭全部标签页</li>',
+                    '<li> 关闭全部标签页关闭全部标签页关闭全部标签页</li>',
+                    '<li> 关闭全部标签页关闭全部标签页关闭全部标签页</li>',
+                    '<li> 关闭全部标签页关闭全部标签页关闭全部标签页</li>',
+                '</ul>',
+                '<div class="layui-tab-content">',
+                    '<div class="layui-tab-item layui-show">',
+                        '<iframe src="{{ d.href }}"></iframe>',
+                    '</div>',
+                '</div>',
+            '</div>'
+        ].join(''),
+        bodySingle: '<iframe src="{{ d.href }}"></iframe>'
+    };
+
+    /**
+     * 布局对象
+     * @constructor
+     */
+    var Layout = function () {
+        var THIS = this,
+            BODY = $('.layui-body'),
+            SIDE = $('.layui-side'),
+            SINGLE = BODY.data('type') === 'single';
+
+        //渲染内容模板
+        laytpl(SINGLE ? Tpl.bodySingle : Tpl.bodyTab).render({
+            title: $.trim(BODY.data('title')),
+            icon: $.trim(BODY.data('icon')),
+            href: $.trim(BODY.data('href'))
+        }, function (html) {
+            BODY.empty().html(html);
+        });
+
+        //当前展示页面的iframe元素
+        this.iframe = BODY.find('iframe').get(0);
+
+
+        if (SINGLE) {
+
+        } else {
+            layui.use('element', function () {
+                var element = layui.element;
+
+                var tabTitle = BODY.find('.layui-tab-title'),
+                    tabTitleWidth = 0,
+                    tabTitleOffset = 0,
+                    tabWidth = 0,
+                    tabDiff = 0,
+                    tabThis = tabTitle.find('.layui-this'),
+                    tabThisWidth = tabThis.outerWidth(),
+                    tabThisLeft = tabThis.position().left,
+                    tabResizeHandle;
+
+                /**
+                 * 重新计算选项卡容器宽度
+                 */
+                function calcTabTitleWidth() {
+                    tabTitleWidth = tabTitle.width();
+                    calcTabDiff();
+                }
+
+                /**
+                 * 重新计算所有的选项卡的宽度和
+                 */
+                function calcTabWidth() {
+                    tabWidth = 0;
+                    tabTitle.find('li').each(function () {
+                        tabWidth += $(this).outerWidth();
+                    });
+                    calcTabDiff();
+                }
+
+                /**
+                 * 重新计算选项卡容器和选项卡标签宽度差
+                 */
+                function calcTabDiff() {
+                    tabDiff = tabTitleWidth - tabWidth;
+                }
+
+                /**
+                 * 设置选项卡容器偏移量
+                 * @param offset 偏移量
+                 */
+                function setOffset(offset) {
+                    var old = tabTitleOffset;
+                    if (offset >= 0 || tabDiff >= 0) {
+                        tabTitleOffset = 0;
+                    } else if (offset < tabDiff) {
+                        tabTitleOffset = tabDiff;
+                    } else {
+                        tabTitleOffset = offset;
+                    }
+                    old === tabTitleOffset || tabTitle.css('left', tabTitleOffset + 'px');
+                }
+
+                /**
+                 * 选项卡往左
+                 * @returns {Layout}
+                 */
+                THIS.tabPrev = function () {
+                    setOffset(tabTitleOffset + tabTitleWidth);
+                    return this;
+                };
+
+                /**
+                 * 选项卡往右
+                 * @returns {Layout}
+                 */
+                THIS.tabNext = function () {
+                    setOffset(tabTitleOffset - tabTitleWidth);
+                    return this;
+                };
+
+                /**
+                 * 窗口改变重新计算
+                 * @param time 延迟执行,毫秒,默认0
+                 * @returns {Layout}
+                 */
+                THIS.resize = function (time) {
+                    tabResizeHandle && clearTimeout(tabResizeHandle);
+                    tabResizeHandle = setTimeout(function () {
+                        calcTabTitleWidth();
+                        if (tabDiff >= 0) {
+                            setOffset(0);
+                        } else if (tabTitleWidth <= tabThisWidth || tabTitleOffset <= -tabThisLeft) {
+                            setOffset(-tabThisLeft);
+                        } else {
+                            setOffset(tabTitleWidth - tabThisLeft - tabThisWidth);
+                        }
+                    }, time || 0);
+                    return this;
+                };
+
+                THIS.tabAdd = function (href, title, icon, id) {
+                    var layid = id || src;
+
+                };
+
+                //初始化选项卡数据
+                calcTabTitleWidth();
+                calcTabWidth();
+
+                //监听选项卡切换
+                element.on('tab(lau-tabs)', function(data) {
+                    tabThis = $(this);
+                    tabThisWidth = tabThis.outerWidth();
+                    tabThisLeft = tabThis.position().left;
+                });
+
+                //监听选项卡关闭
+                element.on('tabDelete(lau-tabs)', function(data) {
+                    tabThisLeft = tabThis.position().left;
+                    calcTabWidth();
+                    THIS.resize();
+                });
+
+                //窗口改变事件
+                $(window).resize(function () {
+                    THIS.resize();
+                });
+
+            });
+
+
+
+
+
+
+
+        }
+
+    };
+
+
+
+    var obj = new Layout();
+    exports('layout', obj);
+    return;
+
+
+
+
+    /**
+     * 选项卡操作类(在选项卡模式下才有用)
+     * @constructor
+     */
+    var Tab = function () {
+        this.tabTitle = BODY_EL.find('.layui-tab-title');
+        this.tabTitleWidth = 0;
+        this.tabWidth = 0;
+        this.diff = 0;
+
+
+
+        /**
+         * 获取选项卡容器宽度
+         * @returns {Tab}
+         */
+        this.getTabTitleWidth = function () {
+            this.tabTitleWidth = this.tabTitle.width();
+            return this;
+        };
+
+        /**
+         * 获取所有选项卡的宽度
+         * @returns {Tab}
+         */
+        this.getTabWidth = function () {
+            var _this = this;
+            _this.tabWidth = 0;
+            _this.tabTitle.find('li').each(function () {
+                _this.tabWidth += $(this).outerWidth();
+            });
+            return _this;
+        };
+
+        /**
+         * 获取选项卡宽度和容器宽度的差
+         * @returns {Tab}
+         */
+        this.getDiff = function () {
+            this.diff = this.tabTitleWidth - this.tabWidth;
+            return this;
+        };
+
+        /**
+         * 设置选项卡容器偏移量
+         * @param offset 偏移量
+         * @returns {Tab}
+         */
+        this.setOffset = function (offset) {
+            console.log(this.offset, this.diff);
+            if (this.offset === offset) {
+                return this;
+            } else if (offset > 0 || this.diff > 0) {
+                this.offset = 0;
+            } else if (offset < 0 && offset < this.diff) {
+                this.offset = this.diff;
+            } else {
+                this.offset = offset;
+            }
+
+            this.tabTitle.css('left', this.offset + 'px');
+            return this;
+        };
+
+        /**
+         * 窗口改变重新计算参数
+         * @returns {Tab}
+         */
+        this.resize = function () {
+            this.getTabTitleWidth().getDiff();
+            if (this.diff > 0) {
+                this.setOffset(0);
+            } else {
+                var _tab = this.tabTitle.find('.layui-this'),
+                    _tabWidth = _tab.outerWidth(),
+                    _tabPos = _tab.position().left,
+                    _d = this.tabTitleWidth - _tabPos - _tabWidth;
+                if (this.offset < -_tabPos) {
+                    this.setOffset(-_tabPos);
+                } else if (_d < 0) {
+                    this.setOffset(_d);
+                } else if (this.offset < this.diff) {
+                    this.setOffset(this.diff);
+                }
+            }
+            return this;
+        };
+
+    };
+
+
+    obj = new Tab();
+    obj.getTabTitleWidth().getTabWidth();
+
+    //监听窗口大小改变
+    $(window).resize(function () {
+        obj.resize();
+    });
+
+    //监听选项卡切换
+    element.on('tab(lau-tabs)', function(data) {
+        console.log(data);
+        var layid = $(this).attr('lay-id'),
+            menu = $('.layui-side .lau-nav-item a[lau-href="' + layid + '"], .layui-side .lau-nav-item a[lau-id="' + layid + '"]');
+        if (menu.length && !menu.next('.lau-nav-child').length) {
+            if (menu.hasClass('lau-nav-header')) {
+                menu.parent().siblings().removeClass('lau-open');
+            } else {
+                var pmenu = menu.parents('.lau-nav-item');
+                pmenu.length && !pmenu.hasClass('lau-open') && pmenu.addClass('lau-open').siblings().removeClass('lau-open');
+            }
+        }
+    });
+
+    exports('layout', obj);
+    return;
+
+
+
 
     /**
      * Layout对象
